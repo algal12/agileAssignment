255d0d87a890698d5a611503fa563a390995d59a
EmmanouilBrotz <ebrotzakis@nyc.gr>
2024-11-19 19:42:04 +0200

    Added initial main.py and started portScanner file

---
+++ portScanner.py
@@ -0,0 +1,6 @@
+# This will be the main interface for Port Scanning. All the functionality will be placed in other classes, this is just an intermediate between them. This will also server as an IP checker.
+
+print("Welcome to the Port Scanner. Please select whether you want TCP, UDP or a TCP/UDP Port Scan.")
+scan_type = input()
+print(scan_type)
+
