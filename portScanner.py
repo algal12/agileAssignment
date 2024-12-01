1caf69d31310a731ac07ce10aaba42ad393cde72
EmmanouilBrotz <ebrotzakis@nyc.gr>
2024-12-01 18:28:23 +0200

    Added UDP Port Scanner

--- portScanner.py
+++ portScanner.py
@@ -1,6 +1,7 @@
 # This will be the main interface for Port Scanning. All the functionality will be placed in other classes, this is just an intermediate between them. This will also server as an IP checker.
 from ipChecker import checkIPType
 from portTCPScanner import get_ports, scan_ports
+from portUDPScanner import scan_udp_ports
 
 print("Welcome to Port Scanner. Please select if you're using an IPv4 or IPv6 address (Type 4 for IPv4, type 6 for IPv6)")
 IP_type = input().strip()  # Will only accept 4 or 6
@@ -17,7 +18,8 @@ else:
             ports = get_ports()
             scan_ports(IP_address, ports)
         elif scan_type.upper() == "UDP":
-            print("going for UDP")
+            ports = get_ports()
+            scan_udp_ports(IP_address, ports)
         elif scan_type.upper() == "TCP/UDP":
             print("going for TCP/UDP")
         else:
